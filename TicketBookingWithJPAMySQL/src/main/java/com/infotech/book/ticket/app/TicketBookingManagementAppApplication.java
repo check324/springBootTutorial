943ea1a45de2812634e6c2de20bd3441d4faa06c
Tianyi Jiang <tianjiang@tesla.com>
2019-04-17 16:48:42 -0700

    sql enabled

--- TicketBookingWithJPAMySQL/src/main/java/com/infotech/book/ticket/app/TicketBookingManagementAppApplication.java
+++ TicketBookingWithJPAMySQL/src/main/java/com/infotech/book/ticket/app/TicketBookingManagementAppApplication.java
@@ -12,27 +12,9 @@ import javax.sql.DataSource;
 import java.util.Date;
 
 @SpringBootApplication
-public class TicketBookingManagementAppApplication implements CommandLineRunner {
-
-	@Autowired
-	private TicketBookingService ticketBookingService;
-
-	@Autowired
-	private DataSource dataSource;
+public class TicketBookingManagementAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TicketBookingManagementAppApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Ticket ticket = new Ticket();
-		ticket.setBookingDate(new Date());
-		ticket.setDestStation("Beijing");
-		ticket.setSourceStation("Changsha");
-		ticket.setPassengerName("Tea");
-		ticket.setEmail("tianjiang@tesla.com");
-		ticketBookingService.createTicket(ticket);
-		System.out.println("dataSource: " + dataSource );
 	}
 }
---
+++ TicketBookingWithJPAMySQL/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+insert into springbootdb.ticket (booking_date, dest_station, email, passenger_name, source_station)
+values (current_timestamp(), 'Beijing', 'tianjiang@tesla.com', 'Tea', 'Changsha');
