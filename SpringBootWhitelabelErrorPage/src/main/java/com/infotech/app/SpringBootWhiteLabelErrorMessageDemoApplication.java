4c248c6ba14f6570de7eaa8f55b13be6294181e5
Tianyi Jiang <tianjiang@tesla.com>
2019-04-26 13:27:22 -0700

    whitelabel error page

---
+++ SpringBootWhitelabelErrorPage/src/main/java/com/infotech/app/SpringBootWhiteLabelErrorMessageDemoApplication.java
@@ -0,0 +1,13 @@
+package com.infotech.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWhiteLabelErrorMessageDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWhiteLabelErrorMessageDemoApplication.class, args);
+    }
+
+}
