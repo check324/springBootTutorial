c607f75a87475625b392be98e805bfdb3bb7c9a6
Tianyi Jiang <tianjiang@tesla.com>
2019-04-26 15:46:18 -0700

    json format error

--- SpringBootDefaultCustomErrorPageReplaceWithJson/src/main/java/com/infotech/app/controller/CustomErrorController.java
+++ SpringBootDefaultCustomErrorPageReplaceWithJson/src/main/java/com/infotech/app/controller/CustomErrorController.java
@@ -1,3 +1,4 @@
+
 package com.infotech.app.controller;
 
 import java.util.Map;
@@ -5,16 +6,22 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.infotech.app.model.ErrorJson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import com.infotech.app.model.ErrorJson;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 
+@RestController
 public class CustomErrorController implements ErrorController {
+
     private static final String PATH = "/error";
 
     @Value("${debug}")
@@ -24,17 +31,18 @@ public class CustomErrorController implements ErrorController {
     private ErrorAttributes errorAttributes;
 
     @RequestMapping(value = PATH)
-    ErrorJson error(HttpServletRequest request, HttpServletResponse response){
-        return new ErrorJson(response.getStatus(), getErrorAttributes(request, debug));
+    ErrorJson error(WebRequest webRequest, HttpServletResponse response) {
+        // Appropriate HTTP response code (e.g. 404 or 500) is automatically set by Spring. 
+        // Here we just define response body.
+        return new ErrorJson(response.getStatus(), getErrorAttributes(webRequest, debug));
     }
 
     @Override
-    public String getErrorPath(){
+    public String getErrorPath() {
         return PATH;
     }
 
-    private Map<String, Object> getErrorAttributes(HttpServletRequest request, boolean debug) {
-        WebRequest webRequest = new ServletWebRequest(request);
-        return errorAttributes.getErrorAttributes(webRequest, debug);
+    private Map<String, Object> getErrorAttributes(WebRequest webRequest, boolean includeStackTrace) {
+        return errorAttributes.getErrorAttributes(webRequest, includeStackTrace);
     }
 }
