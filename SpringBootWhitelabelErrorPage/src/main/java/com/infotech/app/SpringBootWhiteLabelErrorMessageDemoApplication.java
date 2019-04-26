2ffaac87ce3f8fef9f7ecda2df4972d5bb735e51
Tianyi Jiang <tianjiang@tesla.com>
2019-04-26 13:46:08 -0700

    different package

--- SpringBootWhitelabelErrorPage/src/main/java/com/infotech/app/SpringBootWhiteLabelErrorMessageDemoApplication.java
+++ SpringBootWhitelabelErrorPage/src/main/java/com/infotech/app/SpringBootWhiteLabelErrorMessageDemoApplication.java
@@ -2,8 +2,10 @@ package com.infotech.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.infotech.controller")
 public class SpringBootWhiteLabelErrorMessageDemoApplication {
 
     public static void main(String[] args) {
